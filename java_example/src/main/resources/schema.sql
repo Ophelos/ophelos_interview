b54d69cb2bee6cee0e302689f7efe38fcd7147cc
Chia-ling Sung <chia-ling@ophelos.com>
2025-02-21 17:17:28 +0000

    Added Java example.

---
+++ java_example/src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    currency CHAR(3) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "statement" (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES "user"(id),
+    reported_date varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "income" (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    statement_id BIGINT NOT NULL REFERENCES "statement"(id),
+    category VARCHAR(50) NOT NULL,
+    amount int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "expenditure" (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    statement_id BIGINT NOT NULL REFERENCES "statement"(id),
+    category VARCHAR(50) NOT NULL,
+    amount int NOT NULL
+);
+
